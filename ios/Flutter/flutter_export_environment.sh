337aad507e564a8315ccda9772085275b2a19f4b
Roh Geonho <roh517@naver.com>
2020-02-17 17:29:23 +0900

    version update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\roh51\Desktop\Codying\flutter\flutter"
+export "FLUTTER_ROOT=E:\Tools\flutter sdk\flutter"
 export "FLUTTER_APPLICATION_PATH=E:\AndroidStudio\minjok_herald\minjok_herald"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\roh51\Desktop\Codying\flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\Tools\flutter sdk\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
